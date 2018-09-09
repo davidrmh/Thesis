90d36e42c79d7741b6d66876e39af805a7439947
David Montalvan <davidricardo888@hotmail.com>
2018-09-09 00:20:54 -0500

    Add segmentos function

--- sax.py
+++ sax.py
@@ -4,6 +4,7 @@
 
 import scipy.stats
 import numpy as np
+import copy as cp
 
 ##==============================================================================
 ## Función para dividir una distribución normal en N divisiones equiprobables
@@ -41,3 +42,75 @@ def divide_normal(num_divisiones = 2):
         beta[i] = scipy.stats.norm.ppf(float(i + 1) / num_divisiones)
 
     return beta
+
+
+##===========================================================================##
+## Función para obtener los segmentos horizontales representativos de una
+## ventana de datos. (Piecewise Aggregate Approximation)
+## METODOLOGÍA SAX
+##===========================================================================##
+def segmentos(window, num_seg = 3):
+    '''
+    ENTRADA
+    window: numpy array con shape (n,) que contiene los valores de una ventana de tiempo
+
+    num_seg: entero que indica el número de segmentos a obtener
+
+    SALIDA
+    segmentos: Numpy array con los segmentos horizontales
+    window_norm: numpy array con la información de window estandarizada
+    '''
+
+    #Longitud de la ventana
+    n = window.shape[0]
+
+    #Media y desviación estándar
+    mu = window.mean()
+    sigma = window.std(ddof = 1)
+
+    #Estandariza los datos
+    window_norm = (window - mu) / sigma
+
+    #Auxiliar para obtener los segmentos
+    k = int(n / num_seg)
+
+    segmentos = np.zeros(num_seg)
+
+
+    #Caso en que n / num-seg no sea un entero
+    if n % num_seg > 0:
+
+        #parte decimal
+        decimal = n / float(num_seg) - k
+
+        #auxiliares para ponderar los extremos del segmento
+        izq = 1
+        der = decimal
+
+        for i in range(0, num_seg):
+
+            #Obtiene los datos para el cálculo del segmento
+            x = cp.deepcopy(window_norm[i * k:(i + 1) * k + 1])
+
+            #Modifica los extremos
+            x[0] = x[0] * izq
+            x[-1] = x[-1] * der
+
+            #calcula el segmento
+            segmentos[i] = (float(num_seg) / n) * np.sum(x)
+
+            #Actualiza las ponderaciones de los extremos
+            izq = 1 - der
+            der = 1 + decimal - izq
+
+    #Caso en que n / num_seg es un entero
+    else:
+        for i in range(0, num_seg):
+
+            #Obtiene los datos para el cálculo del segmento
+            x = window_norm[i * k:(i + 1) * k]
+
+            #calcula el segmento
+            segmentos[i] = (float(num_seg) / n) * np.sum(x)
+
+    return segmentos, window_norm
