a6b366580de7fd085cf9acb063b4c87cd817b366
David <davidricardo888@hotmail.com>
2018-10-01 17:58:26 -0500

    Add grafica_estrategia.R

---
+++ grafica_estrategia.R
@@ -0,0 +1,7 @@
+datos <- read.csv("naftrac-2017-GA.csv")
+plot(datos$Adj.Close, type = "l", main = "Rojo = Venta \n Blue = Compra \n (Momentos de ejecución)")
+indices_buy <- which(datos$Clase == 1)
+indices_sell <- which(datos$Clase == -1)
+points(x = indices_buy + 1, datos$Adj.Close[indices_buy + 1], col = "blue", pch = 25, cex = 1.5, lwd = 2)
+points(x = indices_sell + 1, datos$Adj.Close[indices_sell + 1], col = "red", pch = 25, cex = 1.5, lwd = 2)
+points(x = length(datos$Adj.Close), y = datos$Adj.Close[length(datos$Adj.Close)], col = "red", pch = 16, cex = 1.5)
