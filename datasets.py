383aca4a967b8c68c630d591d848cabbe3772e57
David <davidricardo888@hotmail.com>
2019-01-24 13:16:47 -0600

    Agrega archivo datasets

---
+++ datasets.py
@@ -0,0 +1,46 @@
+# coding: utf-8
+
+'''
+Funciones para crear los conjuntos de prueba y de entrenamiento
+'''
+
+import indicadores as ind
+import pandas as pd
+import numpy as np
+
+##==============================================================================
+## Función para separar los datos en bloques consecutivos del mismo tamaño
+##==============================================================================
+def separaBloques(datos, lon = 90):
+	'''
+	ENTRADA
+
+	datos: Pandas dataframe con los datos del CSV
+	(idealmente creado con la función leeTabla del módulo indicadores)
+
+	lon: Entero que representa el número de observaciones de cada bloque
+
+	SALIDA
+	Lista con los bloques de datos
+
+	'''
+
+	#número de observaciones
+	n_obs = datos.shape[0]
+
+	#número de bloques posibles
+	n_bloques = int(n_obs / lon)
+
+	#lista para almacenar los bloques
+	bloques = []
+
+	for j in range(0, n_bloques):
+
+		#obtiene el bloque j
+		bloque =  datos.iloc[j*lon:(j+1)*lon,]
+
+		#agrega a la lista
+		bloques.append(bloque)
+
+	return bloques	
+
