e6a66595ca5784a9eabcdb47bd642f70c2a5dfbc
David <davidricardo888@hotmail.com>
2019-02-21 19:29:32 -0600

    Agrega función preciosEjecucion

--- experimentos/auxFun.R
+++ experimentos/auxFun.R
@@ -17,3 +17,61 @@ quitaEspera <- function(datos){
   return(dplyr::filter(datos, Clase != 0))
   
 }
+
+##==============================================================================================
+## Función para calcular el precio de ejecución para un conjunto de fechas dadas
+##
+## ENTRADA
+##  datos: Pandas dataframe con los precios y la columna Date
+##
+##  fechas: Vector con elementos del tipo 'factor' que representa las fechas de ejecución
+##
+##  tipo: String que representa el tipo de precio de ejecución a calcular
+##
+## 	'open': precioEjecucion = Precio de apertura en el día 'fecha' + h
+##
+##  'mid': precioEjecucion = promedio entre High y Low en 'fecha' +  h
+##
+##  'adj.close': precioEjecucion = Cierre ajustado en 'fecha' + h
+##
+##  'close': Precio de Ejecucion = Cierre en 'fecha' + h
+##
+##
+## SALIDA
+##  Vector con el precio de ejecución correspondiente a cada elemento en 'fechas'
+##==============================================================================================
+preciosEjecucion <- function(datos, fechas, tipo = 'open'){
+  
+  #Para almacenar los precios de ejecución
+  preciosEj <- c()
+  
+  for(fecha in fechas){
+    
+    #índice de la observación correspondiente
+    indice <- which(datos$Date == fecha)
+    
+    if(tipo == 'open'){
+      precio <- datos$Open[indice]
+      preciosEj <- c(preciosEj, precio)
+    }
+    
+    else if(tipo == 'mid'){
+      precio <- mean(datos$High[indice], datos$Low[indice])
+      preciosEj <- c(preciosEj, precio)
+    }
+    
+    else if(tipo == 'Adj.Close'){
+      precio <- datos$Adj.Close[indice]
+      preciosEj <- c(preciosEj, precio)
+    }
+    
+    else if(tipo == 'Close'){
+      precio <- datos$Close[indice]
+      preciosEj <- c(preciosEj, precio)
+    }
+    
+  }
+  
+  preciosEj
+}
+
