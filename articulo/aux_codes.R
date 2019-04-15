fb7748970af044436413d047a8afd614895f3bf7
David Montalvan <davidricardo888@hotmail.com>
2019-04-15 17:22:00 -0500

    Agrega aux_codes.R. Archivo con un conjunto de rutinas auxiliares para crear imágenes del artículo

---
+++ articulo/aux_codes.R
@@ -0,0 +1,33 @@
+##==========================================================================================================
+## GRÁFICA DE UN ETIQUETADO
+##==========================================================================================================
+
+#getwd() = carpeta experimentos
+datos <- read.csv("../datasets/etiquetado-sin-rep-mid-h-1-90-sp500/14_sp500_2012-08-22_2013-01-02_90.csv")
+
+#índices de compra y venta
+#NOTA: LOS ÍNDICES SE REFIEREN A LOS ÍNDICES EN LOS QUE SE EJECUTA LA ACCIÓN
+#NO SON LOS ÍNDICES EN LOS QUE SE RECIBE LA SEÑAL
+n_obs <- nrow(datos)
+indices_compra <- which(datos$Clase == 1) + 1
+indices_venta <- which(datos$Clase == -1) + 1
+indices_compra[indices_compra > n_obs] <- n_obs
+indices_venta[indices_venta > n_obs] <- n_obs
+#Remueve las ventas y compras que ocurren el último día del periodo
+if(any(indices_compra == indices_venta)){
+  n_compras <- length(indices_compra)
+  n_ventas <- length(indices_venta)
+  indices_compra <- indices_compra[1:(n_compras - 1)]
+  indices_venta <- indices_venta[1:(n_ventas - 1)]
+}
+
+#precio mid
+datos$Mid <- (datos$High + datos$Low) / 2
+
+#Convierte string de fechas a Date object
+datos$Date <- as.Date(datos$Date)
+
+#gráfica de la estrategia
+plot(datos$Date, datos$Mid, type = "l", lwd = 1.5, xlab = "Date", ylab = "Price", main = "Labeled data")
+points(x = datos$Date[indices_compra], y = datos$Mid[indices_compra], cex = 1.5, pch = 17)
+points(x = datos$Date[indices_venta], y = datos$Mid[indices_venta], cex = 1.5, pch = 25)
