3922675c3e033fc34f29a83f9bdcb98da9d88d46
David Montalvan <davidricardo888@hotmail.com>
2019-05-07 22:11:42 -0500

    Agrega función para graficar buy and hold

--- escrito/aux_codes.R
+++ escrito/aux_codes.R
@@ -1,8 +1,3 @@
-##==========================================================================================================
-## GRÁFICA DE UN ETIQUETADO
-## boolLegend es un boolean. TRUE=>Se dibuja leyenda de forma manual.
-##==========================================================================================================
-
 #getwd() = carpeta experimentos
 datos <- read.csv("../datasets/etiquetado-sin-rep-mid-h-1-90-sp500/14_sp500_2012-08-22_2013-01-02_90.csv")
 
@@ -12,14 +7,27 @@ datos$Mid <- (datos$High + datos$Low) / 2
 #Convierte string de fechas a Date object
 datos$Date <- as.Date(datos$Date)
 
+#Momento de ejecución
+h <- 1
+
+##==========================================================================================================
+## GRÁFICA DE UN ETIQUETADO
+## boolLegend es un boolean. TRUE=>Se dibuja leyenda de forma manual.
+##==========================================================================================================
 graficaEtiquetado <- function(datos, boolLegend = TRUE){
 
+  #precio mid
+  datos$Mid <- (datos$High + datos$Low) / 2
+  
+  #Convierte string de fechas a Date object
+  datos$Date <- as.Date(datos$Date)
+  
   #índices de compra y venta
   #NOTA: LOS ÍNDICES SE REFIEREN A LOS ÍNDICES EN LOS QUE SE EJECUTA LA ACCIÓN
   #NO SON LOS ÍNDICES EN LOS QUE SE RECIBE LA SEÑAL
   n_obs <- nrow(datos)
-  indices_compra <- which(datos$Clase == 1) + 1
-  indices_venta <- which(datos$Clase == -1) + 1
+  indices_compra <- which(datos$Clase == 1) + h
+  indices_venta <- which(datos$Clase == -1) + h
   indices_compra[indices_compra > n_obs] <- n_obs
   indices_venta[indices_venta > n_obs] <- n_obs
   #Remueve las ventas y compras que ocurren el último día del periodo
@@ -52,7 +60,6 @@ graficaEtiquetado <- function(datos, boolLegend = TRUE){
   
 }
 
-
 ##==========================================================================================================
 ## GRÁFICA DE BANDAS HORIZONTALES
 ## indice: Entero que representa el índice de la observación de compra
@@ -63,8 +70,8 @@ graficaBandas <- function(datos, banda_superior = 0.035, banda_inferior = -0.03,
   #NOTA: LOS ÍNDICES SE REFIEREN A LOS ÍNDICES EN LOS QUE SE EJECUTA LA ACCIÓN
   #NO SON LOS ÍNDICES EN LOS QUE SE RECIBE LA SEÑAL
   n_obs <- nrow(datos)
-  indices_compra <- which(datos$Clase == 1) + 1
-  indices_venta <- which(datos$Clase == -1) + 1
+  indices_compra <- which(datos$Clase == 1) + h
+  indices_venta <- which(datos$Clase == -1) + h
   indices_compra[indices_compra > n_obs] <- n_obs
   indices_venta[indices_venta > n_obs] <- n_obs
   #Remueve las ventas y compras que ocurren el último día del periodo
@@ -81,12 +88,12 @@ graficaBandas <- function(datos, banda_superior = 0.035, banda_inferior = -0.03,
   #Convierte string de fechas a Date object
   datos$Date <- as.Date(datos$Date)
   
-  plot(datos$Date, datos$Mid, type = "l", lwd = 2, xlab = "Fecha", ylab = "Precio de ejecución", main = "Límites de venta")
-  points(x = datos$Date[indices_compra[indice]], y = datos$Mid[indices_compra[indice]], cex = 2, pch = 17)
+  plot(datos$Date, datos$Mid, type = "l", lwd = 2.3, xlab = "Fecha", ylab = "Precio de ejecución", main = "Límites de venta")
+  points(x = datos$Date[indices_compra[indice]], y = datos$Mid[indices_compra[indice]], cex = 2, pch = 24, bg = "blue")
   abline(h = datos$Mid[indices_compra[indice]]*(1 + banda_superior), lty = 6, lwd = 1.5)
   abline(h = datos$Mid[indices_compra[indice]]*(1 + banda_inferior), lty = 6, lwd = 1.5)
   grid(lwd=1, col = "lightgray", lty = "dotted")
-  legend(locator(1), legend = c("Compra"), pch = c(17), bty = "n", cex = 1.5, y.intersp = 0.6)
+  legend(locator(1), legend = c("Compra"), pch = c(24), bty = "n", cex = 1.5, y.intersp = 0.6, pt.bg=c("blue"))
   
 }
 
@@ -104,6 +111,56 @@ graficaEstrategiasFolder <- function(ruta = "../experimentos/AQ/AQ_exp_1_dicc1_m
   }
   
 }
+
+##==========================================================================================================
+## GRÁFICA DE BUY AND HOLD
+## boolLegend es un boolean. TRUE=>Se dibuja leyenda de forma manual.
+## utilizar "../datasets/etiquetado-sin-rep-mid-h-1-90-sp500/8_sp500_2010-07-02_2010-11-08_90.csv"
+##==========================================================================================================
+graficaBuyHold <- function(datos, boolLegend = TRUE){
+  
+  #precio mid
+  datos$Mid <- (datos$High + datos$Low) / 2
+  
+  #Convierte string de fechas a Date object
+  datos$Date <- as.Date(datos$Date)
+  
+  #índices de compra y venta
+  #NOTA: LOS ÍNDICES SE REFIEREN A LOS ÍNDICES EN LOS QUE SE EJECUTA LA ACCIÓN
+  #NO SON LOS ÍNDICES EN LOS QUE SE RECIBE LA SEÑAL
+  n_obs <- nrow(datos)
+  indices_compra <- 1 + h
+  indices_venta <- n_obs
+  
+  #Pone la clase en ceros
+  datos$Clase <- 0
+  datos$Clase[indices_compra] <- 1
+  datos$Clase[indices_venta] <- -1
+
+  #gráfica de la estrategia
+  # Guardar en JPEG tamaño 800 x 676
+  main <- "Estrategia buy and hold"
+  
+  if(boolLegend){
+    plot(datos$Date, datos$Mid, type = "l", lwd = 2.3, xlab = "Fecha", ylab = "Precio de ejecución", main = main)
+    points(x = datos$Date[indices_compra], y = datos$Mid[indices_compra], cex = 2, pch = 24, bg = "blue")
+    points(x = datos$Date[indices_venta], y = datos$Mid[indices_venta], cex = 2, pch = 25, bg = "red")
+    lines(x=c(datos$Date[indices_compra], datos$Date[indices_venta]), y = c(datos$Mid[indices_compra], datos$Mid[indices_venta]), lty = 5, lwd = 2.5, col = "darkgreen")
+    grid(lwd=.8, col = "lightgray", lty = "dotted")
+    legend(locator(1), legend = c("Compra", "Venta"), pch = c(24, 25), bty = "n", cex = 1.5, y.intersp = 0.6, pt.bg = c("blue", "red"))  
+  }
+  else{
+    sub <- "Azul = Compra, Rojo = Venta"
+    plot(datos$Date, datos$Mid, type = "l", lwd = 2.0, xlab = "Fecha", ylab = "Precio de ejecución", main = main, sub = sub)
+    points(x = datos$Date[indices_compra], y = datos$Mid[indices_compra], cex = 2, pch = 16, col="blue")
+    points(x = datos$Date[indices_venta], y = datos$Mid[indices_venta], cex = 2, pch = 16, col = "red")
+    lines(x=c(datos$Date[indices_compra], datos$Date[indices_venta]), y = c(datos$Mid[indices_compra], datos$Mid[indices_venta]), lty = 5, lwd = 2.5, col = "darkgreen")
+    grid(lwd=1, col = "lightgray", lty = "dotted")
+  }
+  
+  
+}
+
 
 
 
